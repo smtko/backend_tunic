c8a619f358b941897bd462ba0325c805f5b7a668
smtko <sumitchawda777@gmail.com>
2025-01-19 23:51:50 +0530

    Database connection

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import connectDB from "./db/db.js";
+import dotenv from "dotenv"
+
+dotenv.config({path: './env'})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+( async ()=> {
+    try
+    {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("Error", (error) => {
+            console.log("ERROR: ", error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }
+    catch(error)
+    {
+        console.error("ERROR: ", error)
+        throw error
+    }
+} )()
+*/
